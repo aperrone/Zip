93d72605a5bc424b29070ce8eeb8005187787d1f
Alessandro Perrone <alessandro.perrone@outlook.com>
2022-02-21 13:14:53 +0100

    Remove fullpaths from zip entries

--- Zip.cs
+++ Zip.cs
@@ -6,14 +6,23 @@ namespace System.IO.Compression
 {
     public class Zip
     {
-        public static Task<Stream> CompressAsync(IEnumerable<string> paths)
+        public static Task<Stream> CompressAsync(IEnumerable<string> paths, bool removeFullPaths = true)
         {
-            var streamInfos = paths.Select(name => ((Stream)new FileStream(name, FileMode.Open), name));
+            if (paths is null)
+                throw new ArgumentNullException(nameof(paths));
+
+            var streamInfos = paths.Select(name => ((Stream)new FileStream(name, FileMode.Open), removeFullPaths ? Path.GetFileName(name) : name));
             return CompressAsync(streamInfos);
         }
 
         public static Task<Stream> CompressAsync(Stream stream, string name)
         {
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
+
+            if (name is null)
+                throw new ArgumentNullException(nameof(name));
+
             return CompressAsync(new (Stream Stream, string Name)[] { (stream, name) });
         }
 
@@ -36,14 +45,23 @@ namespace System.IO.Compression
             return output;
         }
 
-        public static Stream Compress(IEnumerable<string> paths)
+        public static Stream Compress(IEnumerable<string> paths, bool removeFullPaths = true)
         {
-            var streamInfos = paths.Select(name => ((Stream)new FileStream(name, FileMode.Open), name));
+            if (paths is null)
+                throw new ArgumentNullException(nameof(paths));
+
+            var streamInfos = paths.Select(name => ((Stream)new FileStream(name, FileMode.Open), removeFullPaths ? Path.GetFileName(name) : name));
             return Compress(streamInfos);
         }
 
         public static Stream Compress(Stream stream, string name)
         {
+            if (stream is null)
+                throw new ArgumentNullException(nameof(stream));
+
+            if (name is null)
+                throw new ArgumentNullException(nameof(name));
+
             return Compress(new (Stream Stream, string Name)[] { (stream, name) });
         }
 
